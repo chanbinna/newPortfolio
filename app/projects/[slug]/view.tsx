2fdc375dd526e7d73d903325be4fa9c733d92084
Chanbin Na <chanbinna@gmail.com>
2024-12-24 02:56:53 -0800

    update

--- app/projects/[slug]/view.tsx
+++ app/projects/[slug]/view.tsx
@@ -1,17 +1,22 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 export const ReportView: React.FC<{ slug: string }> = ({ slug }) => {
-	useEffect(() => {
-		fetch("/api/incr", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({ slug }),
-		});
-	}, [slug]);
+  const hasReported = useRef(false); // Prevent multiple increments
 
-	return null;
+  useEffect(() => {
+    if (hasReported.current) return; // Only increment once
+    hasReported.current = true;
+
+    fetch("/api/incr", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ slug }),
+    });
+  }, [slug]);
+
+  return null;
 };
